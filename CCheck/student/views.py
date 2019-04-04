e4779020036fc9ecbfd6657b89c5de91911345b2
abirhasanmubin <abirhasanmubin@outlook.com>
2019-04-05 01:17:16 +0600

    'as it is'

--- CCheck/student/views.py
+++ CCheck/student/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -62,25 +62,27 @@ class AssignmentDoneCreateView(LoginRequiredMixin, CreateView):
 
 class AssignmentDoneUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = AssignmentDone
-    fields = ['subject', 'assignment', 'content']
+    fields = ['content', ]
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
     def test_func(self):
-        subject = self.get_object()
-        if self.request.user == subject.teacher:
+        ad = self.get_object()
+        if self.request.user == ad.student:
             return True
         return False
 
 
 class AssignmentDoneDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Subject
-    success_url = 'user-assignmentdones'
+    model = AssignmentDone
 
     def test_func(self):
-        subject = self.get_object()
-        if self.request.user == subject.teacher:
+        ad = self.get_object()
+        if self.request.user == ad.student:
             return True
         return False
+
+    def get_success_url(self):
+        return reverse('teacher-subjects')
